8efd6d9e949ce789afdd5f7863e18986d241f947
jmals3 <jmals3@gatech.edu>
2021-05-13 21:51:56 -0400

    added prod build

--- qdn/src/Home.tsx
+++ qdn/src/Home.tsx
@@ -31,7 +31,6 @@ function Home() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Link href="/play">Play</Nav.Link>
-              <Nav.Link href="/download">Download</Nav.Link>
               <Nav.Link href="/blog">Blog</Nav.Link>
               <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
